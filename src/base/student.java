4c37faf0a39f6c0146f4a7d70359eb028fa99d19
busiqiao <busiqiao666@gmail.com>
2021-10-17 13:55:23 +0800

    Base frame

---
+++ src/base/student.java
@@ -0,0 +1,31 @@
+package base;
+
+public class student {
+    private String num = null;
+    private String name = null;
+    private char[][] data = null;
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char[][] getData() {
+        return data;
+    }
+
+    public void setData(char[][] data) {
+        this.data = data;
+    }
+}
---
+++ src/base/timeTable.java
@@ -0,0 +1,11 @@
+package base;
+
+public class timeTable {
+    public static void main(String[] args) {
+        //建立课表类和学生类
+        //提取课表信息，存储到文件
+        //逐条读取文件信息，建立学生对象
+        //读取学生对象，建立课表对象
+        //输出到excel
+    }
+}
